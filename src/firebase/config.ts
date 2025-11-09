8795a7b844bd17e5acd7c54852e1f8ae9fad3c11
HASSAN ALBRAA <hassanalbraa124@gmail.com>
2025-11-09 14:24:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8041995166-63a55",
+  "appId": "1:773195103689:web:b3075f006045346af43c18",
+  "apiKey": "AIzaSyDNsclTn29rarKpa1RzDSvTtq6fgUBm7gU",
+  "authDomain": "studio-8041995166-63a55.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "773195103689"
+};
